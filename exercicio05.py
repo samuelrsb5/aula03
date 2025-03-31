c9667acc71f7bad8e0b84b659638cb97ad82ea83
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-03-31 16:24:44 -0300

    Correção no ex. 5, nota 3 adicionada

--- exercicio05.py
+++ exercicio05.py
@@ -1,8 +1,9 @@
 nome_aluno = input("Informe seu nome: ")
 nota_1 = int(input("informe a nota 1: "))
 nota_2 = int(input("informe a nota 2: "))
+nota_3 = int(input("informe a nota 3: "))
 
-media = (nota_1 + nota_2) / 2
+media = (nota_1 + nota_2 + nota_3) / 3
 
 if media >= 7:
     print(f"{nome_aluno}, sua média é {media} e você está aprovado")
