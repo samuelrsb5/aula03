e6803cda912ab1c43b93bb7f66c211ff4f962eff
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-03-31 17:08:53 -0300

    Enviando atualizações finais e correções

--- exercicio06.py
+++ exercicio06.py
@@ -1,9 +1,13 @@
-time_1 = float(input("Quantos gols o time 1 fez? "))
-time_2 = float(input("Quantos gols o time 2 fez? "))
+nome_time1 = input("Qual o nome do time 1? ")
+nome_time2 = input("Qual o nome do time 2? ")
 
-if time_1 > time_2:
-    print("Time 1 ganhou")
-if time_2 > time_1:
-    print("Time 2 ganhou")
-if time_1 == time_2:
-    print("Empate")
+gols_time1 = float(input("Quantos gols o time 1 fez? "))
+gols_time2 = float(input("Quantos gols o time 2 fez? "))
+
+if gols_time1 > gols_time2:
+    print(f"{nome_time1} ganhou com {gols_time1} gols")
+else:
+    if gols_time2 > gols_time1:
+        print(f"{nome_time2} ganhou com {gols_time2} gols")
+    else:
+        print(f"{nome_time1} e {nome_time2} empataram")
