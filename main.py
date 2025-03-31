cdf4b7d5341a97fd9748e46329c7e324e870ddac
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-03-31 14:33:01 -0300

    Segunda parte do exercicio 1 da aula 3

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 nome = input("Informe seu nome: ")
 idade = int(input("Informe sua idade: "))
 salario = float(input("Informe seu salario: "))
+porcentagemAcrescimo = float(input("Informe uma porcetangem de acrescimo: "))
 
-print(f"Seu nome é {nome}, tem {idade} anos e ganha R${salario}")
+valorAcrescimo = (salario * porcentagemAcrescimo) / 100
+
+salarioAtualizado = salario + valorAcrescimo
+
+# print(f"Seu nome é {nome}, tem {idade} anos e ganha R${salario}")
+
+print(f"Nome: {nome}\nIdade: {idade}\nRecebeu um acrescimo de {porcentagemAcrescimo}% que equivalem a R${valorAcrescimo}.\nSeu salário atualmente é de R${salarioAtualizado}.")
 
 
 
