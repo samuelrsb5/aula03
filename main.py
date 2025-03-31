d88d87a2f0a5a8f9a32aeac82042450fc512374b
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-03-31 14:11:12 -0300

    Enviando o exercicio 1 da aula 3

---
+++ main.py
@@ -0,0 +1,8 @@
+nome = input("Informe seu nome: ")
+idade = int(input("Informe sua idade: "))
+salario = float(input("Informe seu salario: "))
+
+print(f"Seu nome é {nome}, tem {idade} anos e ganha R${salario}")
+
+
+
