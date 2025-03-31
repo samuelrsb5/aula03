7aeb1c10ed32e7ab1d337e05f50e8061a4199a20
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-03-31 16:51:41 -0300

    Enviando atualizações

--- exercicio05.py
+++ exercicio05.py
@@ -1,13 +1,14 @@
 nome_aluno = input("Informe seu nome: ")
-nota_1 = int(input("informe a nota 1: "))
-nota_2 = int(input("informe a nota 2: "))
-nota_3 = int(input("informe a nota 3: "))
+nota_1 = float(input("informe a nota 1: "))
+nota_2 = float(input("informe a nota 2: "))
+nota_3 = float(input("informe a nota 3: "))
 
 media = (nota_1 + nota_2 + nota_3) / 3
 
+# :.2f para formatar o número
 if media >= 7:
-    print(f"{nome_aluno}, sua média é {media} e você está aprovado")
+    print(f"{nome_aluno}, sua média é {media:.2f} e você está aprovado")
 if media < 4:
-    print(f"{nome_aluno}, sua média é {media} e você está reprovado")
+    print(f"{nome_aluno}, sua média é {media:.2f} e você está reprovado")
 else:
-    print((f"{nome_aluno}, sua média é {media} e você irá para a recuperação"))
+    print((f"{nome_aluno}, sua média é {media:.2f} e você irá para a recuperação"))
