e98b50e075a5fc19342109b07752d390c5be6e7c
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-03-31 14:54:26 -0300

    Enviando exercicio 2 da aula 3

---
+++ exercicio02.py
@@ -0,0 +1,12 @@
+numero1 = float(input("Informe um número: "))
+numero2 = float(input("Informe outro número: "))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print(f"{numero1} + {numero2} = {soma}")
+print(f"{numero1} - {numero2} = {subtracao}")
+print(f"{numero1} * {numero2} = {multiplicacao}")
+print(f"{numero1} / {numero2} = {divisao}")
