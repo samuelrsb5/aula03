1150ad0abf7aca0199f1b9caae5aafe7cf5c5cc3
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-03-31 16:15:00 -0300

    Enviando exercicio 4 da aula 3 e comentário no ex. 3

--- exercicio03.py
+++ exercicio03.py
@@ -4,6 +4,7 @@ B = 10
 print(f"A: {A}")
 print(f"B: {B}")
 
+# X é uma variável auxiliar, que vai servir de balde vazio para armazenar um valor de outra variável
 X = A
 A = B
 B = X
---
+++ exercicio04.py
@@ -0,0 +1,7 @@
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+
+if numero_1 < numero_2:
+    print(f"{numero_1} e {numero_2}")
+else:
+    print(f"{numero_2} e {numero_1}")
