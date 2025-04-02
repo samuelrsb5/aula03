1833c5fe7e2106a20c80036c17bd4d3311cda2ce
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-04-02 14:32:28 -0300

    Enviando a parte 2 do ex. 7 da aula 3

--- exercicicio07.py
+++ exercicicio07.py
@@ -1,16 +1,17 @@
+
 quantidadeLitros = float(input("Informe quantos litros irá abastecer: "))
 tipoCombustivel = input("Informe o tipo de combustível: ")
 
 gasolina = 5.80
 etanol = 4.90
 
-abastecimentoGasolina = quantidadeLitros*gasolina
-abastecimentoEtanol = quantidadeLitros*etanol
 
 
-if tipoCombustivel == "G":
+if tipoCombustivel == "G" or tipoCombustivel == "g":
+    abastecimentoGasolina = quantidadeLitros * gasolina
     print(f"O valor do abastecimento é de R${abastecimentoGasolina:.2f}")
-elif tipoCombustivel == "E":
+elif tipoCombustivel == "E" or tipoCombustivel == "e":
+    abastecimentoEtanol = quantidadeLitros * etanol
     print(f"O valor do abastecimento é de R${abastecimentoEtanol:.2f}")
 else:
     print("Tipo inválido, informe apenas G(gasolina) ou E(etanol)")
