6e47e1f7875e6e12292b5365438c82d932f5d78d
samuelrsb5 <samuelcomgeorge@gmail.com>
2025-04-07 15:32:06 -0300

    enviando ex. 09 da aula 03

---
+++ exercicio09.py
@@ -0,0 +1,32 @@
+numero = int(input("Informe um número: "))
+
+
+
+
+if numero < 1 or numero > 12:
+    print("Valor Inválido")
+elif numero == 1:
+    print("Janeiro")
+elif numero == 2:
+    print("Fevereiro")
+elif numero == 3:
+    print("Março")
+elif numero == 4:
+    print("Abril")
+elif numero == 5:
+    print("Maio")
+elif numero == 6:
+    print("Junho")
+elif numero == 7:
+    print("Julho")
+elif numero == 8:
+    print("Agosto")
+elif numero == 9:
+    print("Setembro")
+elif numero == 10:
+    print("Outubro")
+elif numero == 11:
+    print("Novembro")
+else:
+    print("Dezembro")
+    
